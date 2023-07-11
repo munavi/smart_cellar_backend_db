2d0c183bd7bfd2ca39889dd7690a649005164195
Natalya Murashko <natalya.murashko@hs-augsburg.de>
2023-07-11 15:53:59 +0200

    feat: express with orm sequelize work, create db tables, add cors

---
+++ db.js
@@ -0,0 +1,12 @@
+const {Sequelize} = require('sequelize')
+
+module.exports = new Sequelize(
+    process.env.PG_DATABASE,
+    process.env.PG_USER,
+    process.env.PG_PASSWORD,
+    {
+        dialect: 'postgres',
+        host: process.env.PG_HOST,
+        port: process.env.PG_PORT
+    }
+)
---
+++ models/models.js
@@ -0,0 +1,50 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.UUID, primaryKey: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password : {type: DataTypes.STRING}
+})
+
+const Product = sequelize.define('product', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false}, // should be unique?
+    counter : {type: DataTypes.INTEGER, defaultValue: 0},
+    date: {type: DataTypes.DATE, allowNull: false},
+})
+
+const Category = sequelize.define('category', {
+    id: {type: DataTypes.INTEGER, primaryKey: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+
+const StorageLocation = sequelize.define('storage_location', {
+    id: {type: DataTypes.INTEGER, primaryKey: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+
+User.hasMany(Product)
+Product.belongsTo(User)
+
+Product.hasOne(User)
+User.belongsTo(Product)
+
+Product.hasOne(Category)
+Category.belongsTo(Product)
+
+Product.hasOne(StorageLocation)
+StorageLocation.belongsTo(Product)
+
+Category.hasMany(Product)
+Product.belongsTo(Category)
+
+StorageLocation.hasMany(Product)
+Product.belongsTo(StorageLocation)
+
+module.exports = {
+    User,
+    Product,
+    Category,
+    StorageLocation
+}
